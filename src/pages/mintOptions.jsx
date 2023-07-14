700a12d566d07e07c22861adca309494a0a26269
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-07-14 19:08:21 +0530

    page 2 added

--- src/pages/mintOptions.jsx
+++ src/pages/mintOptions.jsx
@@ -1,13 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
-// import { walletAddress } from "../component/walletConnect/wallet";
+import { useNavigate } from "react-router-dom";
+import "./mintOptions.css";
 
-function MintOptions(props) {
+function MintOptions() {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const [walletAddress, setWalletAddress] = useState(location.state.wallet);
+
+  function mintFromURL() {
+    navigate("/urlnft", {
+      state: {
+        wallet: walletAddress,
+      },
+    });
+  }
+
+  function mintFromAI() {
+    navigate("/ainft", {
+      state: {
+        wallet: walletAddress,
+      },
+    });
+  }
 
   return (
     <div>
-      <h1>{location.state.wallet}</h1>
+      <div className="navBar">
+        <div className="navBarText">NFTx</div>
+        <div className="navBarText">Hello {walletAddress}!</div>
+      </div>
+      <h1 className="Heading">How do you want to generate NFT Image?</h1>
+      <h3 className="HeadingText">Choose an option to mint an NFT.</h3>
+      <div className="optionButton">
+        <button onClick={mintFromURL}>Mint an NFT from URL</button>
+        <button onClick={mintFromAI}>Mint an NFT using Generative AI</button>
+      </div>
     </div>
   );
 }
